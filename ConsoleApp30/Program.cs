c5908620d9d565e8315e275a60e8d8900579722d
lenya <hellchildgg@mail.ru>
2022-12-23 14:48:51 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double number, summa = 0;
+                Console.WriteLine("Для поиска суммы отрицательных чисел закончите ввод значений, значением : 0");
+                Console.WriteLine("Введите значения через Enter: ");
+                do
+                {
+                    number = double.Parse(Console.ReadLine());
+
+                    if (number > 0) continue;
+
+                    summa += number;
+                } while (number != 0);
+                Console.WriteLine("Сумма отрицательных чисел: " + summa);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
